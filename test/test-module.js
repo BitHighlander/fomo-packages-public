1fee03d26ad148e09270542eb05b30498a8e1151
highlander <bithighlander@gmail.com>
2019-07-05 21:32:02 -0600

    init

---
+++ test/test-module.js
@@ -0,0 +1,10 @@
+// const coincap = require('coincap-lib')
+// coincap.open()
+// coincap.on('trades', console.log)
+
+// require('dotenv').config({path:"../.env"});
+// let log = require("../loggerdog-client/")()
+//
+// let TAG = "test"
+//
+// log.info(TAG," wuddup",{my:"nigga"})
